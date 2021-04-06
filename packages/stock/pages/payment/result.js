0d8d557156ca83c13419874a807df66a8993ebb0
gocreating <gocreating@gmail.com>
2021-04-06 18:11:47 +0800

    Add payment result page for stock app

---
+++ packages/stock/pages/payment/result.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import Alert from 'react-bootstrap/Alert'
+import Button from 'react-bootstrap/Button'
+import { StatusEnum } from '@lation/utils/constants/index'
+import AppLayout from '../../components/AppLayout'
+import { withTranslation } from '../../i18n'
+
+const PaymentResultPage = ({ t }) => {
+  const router = useRouter()
+  const [paymentDescription, setPaymentDescription] = useState('')
+  const [isPaymentSuccess, setIsPaymentSuccess] = useState()
+  const query = router.query
+  useEffect(() => {
+    if (query.status === `${StatusEnum.SUCCESS}`) {
+      setIsPaymentSuccess(true)
+    } else if (query.status === `${StatusEnum.FAILED}`) {
+      setIsPaymentSuccess(false)
+      setPaymentDescription(query.error)
+    }
+  }, [query])
+  return (
+    <AppLayout title={t('paymentResult:title')} noAd>
+      {isPaymentSuccess !== undefined && (
+        isPaymentSuccess ? (
+          <Alert variant="success">
+            <Alert.Heading>{t('paymentResult:paymentSuccess.title')}</Alert.Heading>
+            <p>{t('paymentResult:paymentSuccess.description')}</p>
+            <hr />
+            <Link href="/me/subscription" passHref>
+              <Button variant="outline-success">
+                {t('paymentResult:paymentSuccess.cta')}
+              </Button>
+            </Link>
+          </Alert>
+        ) : (
+          <Alert variant="danger">
+            <Alert.Heading>{t('paymentResult:paymentError.title')}</Alert.Heading>
+            <p>{paymentDescription}</p>
+            <hr />
+            <Link href="/product" passHref>
+              <Button variant="secondary">
+                {t('paymentResult:paymentError.cta')}
+              </Button>
+            </Link>
+          </Alert>
+        )
+      )}
+    </AppLayout>
+  )
+}
+
+PaymentResultPage.getInitialProps = async () => ({
+  namespacesRequired: ['common', 'paymentResult'],
+})
+
+export default withTranslation(['common', 'paymentResult'])(PaymentResultPage)
